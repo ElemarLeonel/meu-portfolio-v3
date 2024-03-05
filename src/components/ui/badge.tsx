bb15b7e6f93db9298bd0a2b9db8934379ec123df
Elemar Leonel <elemarleonelbalduino@gmail.com>
2024-03-05 09:47:21 -0400

    feat: Add new properties for Badge component

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,23 +1,25 @@
-import * as React from "react"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-full border border-slate-200 px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 dark:border-slate-800 dark:focus:ring-slate-300",
+  "inline-flex items-center rounded-full border border-slate-200 font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 dark:border-slate-800 dark:focus:ring-slate-300",
   {
     variants: {
       variant: {
         default:
-          "bg-gradient-to-t from-orange-500 to-orange-300 text-orange-900 font-medium font-circularstd border-[1.5px] border-orange-600 rounded-full",
-        outline: "bg-blue-900 text-white uppercase font-medium font-circularstd border border-orange-600 rounded-full",
+          "bg-gradient-to-b from-orange-500 to-orange-300 px-6 py-1.5 text-base text-orange-900 font-medium font-circularstd border-[1.5px] border-orange-600 rounded-full",
+        outline:
+          "bg-blue-900 text-white uppercase font-medium px-6 py-1.5 text-base font-circularstd border border-orange-600 rounded-full",
+        icon: "bg-blue-600 text-white font-circularstd px-6 py-3 border-[3px] border-blue-600 rounded-full px-6 py-3 gap-2.5 text-base",
       },
     },
     defaultVariants: {
       variant: "default",
     },
   }
-)
+);
 
 export interface BadgeProps
   extends React.HTMLAttributes<HTMLDivElement>,
@@ -26,7 +28,7 @@ export interface BadgeProps
 function Badge({ className, variant, ...props }: BadgeProps) {
   return (
     <div className={cn(badgeVariants({ variant }), className)} {...props} />
-  )
+  );
 }
 
-export { Badge, badgeVariants }
+export { Badge, badgeVariants };
