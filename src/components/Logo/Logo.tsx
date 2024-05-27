c1dca1a9e8b9adc3ab4bba471b84fdbf37b51e96
ElemarLeonel <elemarleonelbalduino@gmail.com>
2024-05-26 23:07:29 -0400

    feat: Add new parameters for logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,10 +1,14 @@
-export default function Logo() {
+export default function Logo({ color, size }: { color: string; size: string }) {
   return (
     <div className="flex flex-row gap-1">
-      <h1 className="hidden md:inline font-bold font-spacegrotesk text-xl text-white">
+      <h1
+        className={`hidden md:inline font-bold font-spacegrotesk text-${size} text-${color}`}
+      >
         elemarleonel<em className="text-orange-500">.</em>dev
       </h1>
-      <h1 className="inline md:hidden font-bold font-spacegrotesk text-xl text-white">
+      <h1
+        className={`inline md:hidden font-bold font-spacegrotesk text-${size} text-${color}`}
+      >
         el.d
       </h1>
     </div>
